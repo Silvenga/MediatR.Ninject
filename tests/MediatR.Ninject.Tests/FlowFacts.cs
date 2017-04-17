383a004516577d8f35d99bf341d552697ac6e44a
Mark Lopez <m@silvenga.com>
2017-04-17 14:23:29 -0500

    Added tests project.

---
+++ tests/MediatR.Ninject.Tests/FlowFacts.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace MediatR.Ninject.Tests
+{
+    public class FlowFacts
+    {
+        [Fact]
+        public void METHOD_NAME()
+        {
+            
+            // Act
+
+            // Assert
+        }
+    }
+}
